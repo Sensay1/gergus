292ac30cada8b47f125a7dc5f9a7b1e2802a52f5
gerhard <gerhard.gustavsen@outlook.com>
2022-03-11 11:54:39 +0100

    updated links

--- src/components/articles/Links.js
+++ src/components/articles/Links.js
@@ -7,19 +7,19 @@ import cv from '../../resources/svg/cv.svg'
 
 export const Links = () => (
     <div class="links">
-        <a href="https://www.linkedin.com/" class="underline">
+        <a href="https://www.linkedin.com/in/gerhard-gustavsen-237146234/" class="underline">
             <img src={linkedIn} alt="" />
             LinkedIn
         </a>
-        <a href="https://www.github.com/" class="underline">
+        <a href="https://github.com/Sensay1" class="underline">
             <img src={gitHub} alt="" />
             GitHub
         </a>
-        <a href="https://www.linkedin.com/" class="underline">
+        <a href="mailto:gerhard.gustavsen@outlook.com" class="underline">
             <img src={mail} alt="" />
             Mail
         </a>
-        <a href="https://www.linkedin.com/" class="underline">
+        <a href="https://folk.ntnu.no/gerhargu/CV/CV.pdf" class="underline">
             <img src={cv} alt="" />
             CV
         </a>
